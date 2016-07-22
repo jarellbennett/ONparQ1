f83093da1aa34744e6ebe4872b951a9d90d4a728
Jarell Bennett <jb123506@gmail.com>
2016-07-21 20:42:01 -0400

    first take

---
+++ OnparQ1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnparQ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rePlay = "yes";         // while statement string condition
+
+            while(rePlay == "yes")
+            {
+                Console.WriteLine("Hello" + '\n' + "Please Enter a number to sum");
+                int numbah = Convert.ToInt32(Console.ReadLine());
+
+                int sum = 0;
+                sum += numbah;
+                Console.WriteLine("The number(s) you have entered are " + store + " with a total of " + sum);
+                Console.WriteLine("Would you like to enter another number?");
+                rePlay = Console.ReadLine().ToLower();
+            }
+        }
+    }
+}
